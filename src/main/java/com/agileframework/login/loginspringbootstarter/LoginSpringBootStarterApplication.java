d1031540d072721f999da200bf474e1bf7dabae2
hzmnue <79228515@qq.com>
2018-12-22 10:46:15 +0800

    Initial commit

---
+++ src/main/java/com/agileframework/login/loginspringbootstarter/LoginSpringBootStarterApplication.java
@@ -0,0 +1,14 @@
+package com.agileframework.login.loginspringbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginSpringBootStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginSpringBootStarterApplication.class, args);
+	}
+
+}
+
